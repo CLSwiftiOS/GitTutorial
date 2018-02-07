0277a1441f1c50eaeea556ebe9f74680fb9ec3d9
Christian Liefeldt <liefeldt@markuss-imac.local>
2018-02-07 13:01:21 +0100

    Initial Commit

---
+++ GitxCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitxCode
+//
+//  Created by Christian Liefeldt on 07.02.18.
+//  Copyright © 2018 CL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
